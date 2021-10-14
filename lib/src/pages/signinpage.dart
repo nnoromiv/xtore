5bfd2e668092a0d10ae6249f5204616afb60c896
nnorom-coder <nnorom.prince44@gmail.com>
2021-10-14 18:06:57 +0100

    master

--- lib/src/pages/signinpage.dart
+++ lib/src/pages/signinpage.dart
@@ -7,7 +7,37 @@ class SignInPage extends StatefulWidget {
   _SignInPageState createState() => _SignInPageState();
 }
 
+bool _toggleVisibility = true;
+
 class _SignInPageState extends State<SignInPage> {
+  Widget _buildEmailTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          hintText: "Email or Username",
+          hintStyle: TextStyle(color: Colors.grey, fontSize: 16.0)),
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: "Password",
+        hintStyle: TextStyle(color: Colors.grey, fontSize: 16.0),
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              _toggleVisibility = !_toggleVisibility;
+            });
+          },
+          icon: _toggleVisibility
+              ? Icon(Icons.visibility_off_outlined)
+              : Icon(Icons.visibility_outlined),
+        ),
+      ),
+      obscureText: _toggleVisibility,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +52,71 @@ class _SignInPageState extends State<SignInPage> {
                   fontWeight: FontWeight.bold,
                   fontSize: 30.0,
                   color: Colors.black),
+            ),
+            SizedBox(
+              height: 100.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "Forgotten Password",
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    _buildEmailTextField(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    _buildPasswordTextField(),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              height: 50.0,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.lightGreen),
+              child: Center(
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ),
+            ),
+            Divider(
+              height: 40.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account?",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  "Signup",
+                  style: TextStyle(
+                      color: Colors.lightGreen, fontWeight: FontWeight.bold),
+                ),
+              ],
             )
           ],
         ),
